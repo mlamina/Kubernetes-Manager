244f712e7d084ff8e5688181922591790014df00
Marco Lamina <mlamina09@gmail.com>
2015-10-30 19:15:44 +0100

    Configured auto update for CPU / Memory usage in node detail view

--- app/components/nodes/node-detail.js
+++ app/components/nodes/node-detail.js
@@ -2,70 +2,99 @@
 
 angular.module('k8s-manager.nodes')
 
-.controller('NodeDetailCtrl', ['$scope', '$stateParams', 'Nodes', 'details', 'containers',
-  function($scope, $stateParams, Nodes, details, containers) {
-    $scope.nodeName = $stateParams.name;
-    $scope.details = details;
-    $scope.containers = containers;
-    function reloadData() {
-      Nodes.getDetails($stateParams.name).then(function(details) {
-        $scope.details = details;
-      });
-      Nodes.getContainers($stateParams.name).then(function(containers) {
-        $scope.containers = containers;
-      });
-    }
-    $scope.$on('auto-reload', reloadData);
-    // Calculate file system usages
-    var usages = {};
-    angular.forEach(containers.stats[containers.stats.length-1].filesystem, function(fs) {
-      var percentage = fs.capacity / fs.usage;
-      var type = 'success';
-      if (percentage < 25) {
-        type = 'success';
-      } else if (value < 50) {
-        type = 'info';
-      } else if (value < 75) {
-        type = 'warning';
-      } else {
-        type = 'danger';
+  .controller('NodeDetailCtrl', ['$scope', '$stateParams', 'Nodes', 'details', 'containers',
+    function($scope, $stateParams, Nodes, details, containers) {
+      $scope.nodeName = $stateParams.name;
+      $scope.details = details;
+      $scope.containers = containers;
+      function reloadData() {
+        Nodes.getDetails($stateParams.name).then(function(details) {
+          $scope.details = details;
+        });
+        Nodes.getContainers($stateParams.name).then(function(containers) {
+          $scope.containers = containers;
+        });
       }
-      fs.percentage = percentage;
-      fs.type = type;
-      usages[fs.device] = fs;
-    });
-    $scope.fsStats = usages;
-
-    // Configure CPU chart
-    $scope.cpuChartData = [];
-    var cpuCapacityPerSecond = details.num_cores * 1000000000; // cores * nanoseconds
-    for (var i=1; i < containers.stats.length; i++) {
-      var cpuUsage = containers.stats[i].cpu.usage.total - containers.stats[i-1].cpu.usage.total;
-      $scope.cpuChartData.push({
-        date: new Date(containers.stats[i].timestamp),
-        usage: (cpuUsage / cpuCapacityPerSecond) * 100
+      $scope.$on('auto-reload', reloadData);
+      // Calculate file system usages
+      var usages = {};
+      angular.forEach(containers.stats[containers.stats.length-1].filesystem, function(fs) {
+        var percentage = fs.capacity / fs.usage;
+        var type = 'success';
+        if (percentage < 25) {
+          type = 'success';
+        } else if (value < 50) {
+          type = 'info';
+        } else if (value < 75) {
+          type = 'warning';
+        } else {
+          type = 'danger';
+        }
+        fs.percentage = percentage;
+        fs.type = type;
+        usages[fs.device] = fs;
       });
-    }
+      $scope.fsStats = usages;
 
-    $scope.cpuChartOptions = {
-      axes: {
-        x: {key: 'date', type: 'date', thickness: '2px', ticks: 2},
-        y: {type: 'linear', min: 0, max: 100, ticks: 5, innerTicks: true, grid: true}
-      },
-      margin: {
-        left: 30,
-        right: 10,
-        bottom: 30
-      },
-      series: [
-        {y: 'usage', color: 'steelblue', thickness: '2px', type: 'area', striped: false, label: 'CPU Utilization', drawDots: false}
-      ],
-      lineMode: 'linear',
-      tension: 0.7,
-      //tooltip: {mode: 'scrubber', formatter: function(x, y, series) {return 'pouet';}},
-      drawLegend: false,
-      drawDots: false,
-      hideOverflow: false,
-      columnsHGap: 5
-    }
-  }]);
+      $scope.$watch('containers', function(newValue, oldValue) {
+        configureCpuChart();
+      });
+
+      function configureCpuChart() {
+        // Configure CPU chart
+        $scope.cpuChartData = [];
+        var cpuCapacityPerSecond = $scope.details.num_cores * 1000000000; // cores * nanoseconds
+        for (var i = 1; i < $scope.containers.stats.length; i++) {
+          var cpuUsage = $scope.containers.stats[i].cpu.usage.total - $scope.containers.stats[i - 1].cpu.usage.total;
+          $scope.cpuChartData.push({
+            date: new Date($scope.containers.stats[i].timestamp),
+            cpuUsage: (cpuUsage / cpuCapacityPerSecond) * 100,
+            memoryUsage: ($scope.containers.stats[i].memory.usage / $scope.details.memory_capacity) * 100
+          });
+        }
+        $scope.cpuChartOptions = {
+          axes: {
+            x: {key: 'date', type: 'date', thickness: '2px', ticks: 3},
+            y: {type: 'linear', min: 0, max: 100, ticks: 5, innerTicks: true, grid: true}
+          },
+          margin: {
+            left: 30,
+            right: 20,
+            bottom: 30
+          },
+          series: [
+            {
+              y: 'cpuUsage',
+              color: 'steelblue',
+              thickness: '2px',
+              type: 'line',
+              striped: false,
+              label: 'CPU Utilization',
+              drawDots: false
+            },
+            {
+              y: 'memoryUsage',
+              color: '#FF6633',
+              thickness: '1px',
+              type: 'area',
+              striped: false,
+              label: 'Memory Utilization',
+              drawDots: false
+            }
+          ],
+          lineMode: 'linear',
+          tension: 0.7,
+          tooltip: {
+            mode: 'scrubber',
+            formatter: function (x, y, series) {
+              return series.label + ': ' + Math.round(y) + '%';
+            }
+          },
+          drawLegend: false,
+          drawDots: false,
+          hideOverflow: false,
+          columnsHGap: 5
+        }
+      }
+      configureCpuChart();
+    }]);
