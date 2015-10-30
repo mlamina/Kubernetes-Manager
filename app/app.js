54a0b737c0a6313d1302394833ba2981faa408e1
Marco Lamina <mlamina09@gmail.com>
2015-10-30 09:39:28 +0100

    Added file system usage

--- app/app.js
+++ app/app.js
@@ -114,4 +114,13 @@ angular.module('k8s-manager', [
       setAutoReloadInterval();
     };
     setAutoReloadInterval();
-  }]);
+  }])
+  .filter('bytes', function() {
+    return function(bytes, precision) {
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+      if (typeof precision === 'undefined') precision = 1;
+      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+        number = Math.floor(Math.log(bytes) / Math.log(1024));
+      return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+    }
+  });
--- app/components/nodes/node-detail.js
+++ app/components/nodes/node-detail.js
@@ -16,4 +16,23 @@ angular.module('k8s-manager.nodes')
       });
     }
     $scope.$on('auto-reload', reloadData);
+    // Calculate file system usages
+    var usages = {};
+    angular.forEach(containers.stats[containers.stats.length-1].filesystem, function(fs) {
+      var percentage = fs.capacity / fs.usage;
+      var type = 'success';
+      if (percentage < 25) {
+        type = 'success';
+      } else if (value < 50) {
+        type = 'info';
+      } else if (value < 75) {
+        type = 'warning';
+      } else {
+        type = 'danger';
+      }
+      fs.percentage = percentage;
+      fs.type = type;
+      usages[fs.device] = fs;
+    });
+    $scope.fsStats = usages;
   }]);
