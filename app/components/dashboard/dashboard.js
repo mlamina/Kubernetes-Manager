a9999fc710aa9fb9607c0efd08b037cff4a0bf68
Marco Lamina <mlamina09@gmail.com>
2016-04-16 12:45:29 +0200

    Added tabs for namespace selection

--- app/components/dashboard/dashboard.js
+++ app/components/dashboard/dashboard.js
@@ -5,7 +5,8 @@ angular.module('k8s-manager.dashboard', ['k8s-manager.api', 'ui.router'])
   .controller('DashboardController', function($scope, $state, $stateParams, namespaces) {
     $scope.namespaces = namespaces;
     $scope.currentNamespace = $stateParams.namespace || 'default';
-    $scope.namespaceSelected = function() {
-      $state.go('app.dashboard', {namespace: $scope.currentNamespace});
+    $scope.namespaceSelected = function(namespace) {
+      debugger;
+      $state.go('app.dashboard', {namespace: namespace});
     };
   });
